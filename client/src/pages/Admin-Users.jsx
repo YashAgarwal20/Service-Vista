74b25d1bee0904d5a287e192fc1562dbe5827074
yash <yash201802@gmail.com>
2024-03-13 09:29:10 +0530

    Admin Authorization

--- client/src/pages/Admin-Users.jsx
+++ client/src/pages/Admin-Users.jsx
@@ -1,5 +1,31 @@
+import { useEffect } from "react";
+import {useAuth} from "../store/Auth";
 const AdminUsers=()=>
 {
+    const {token}=useAuth();
+    const getAllUsersData=async()=>
+    {
+        try {
+            const response=await fetch("http://localhost:3000/api/admin/users",{
+                method:"GET",
+                headers:{
+                    Authorization:`BEARER ${token}`,
+                }
+            });
+            const data=await response.json();
+            console.log(data);
+
+            
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+
+  useEffect(()=>{
+    getAllUsersData();
+  },[]);
+
    return(
     <h1>user admin</h1>
    )
