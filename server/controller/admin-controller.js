e05bb38b873c925c93492626a11dbf1beacb0553
yash <yash201802@gmail.com>
2024-03-14 23:12:51 +0530

    User Data Updates in admin panel,react form handling,toast alerts amd smooth interactions

--- server/controller/admin-controller.js
+++ server/controller/admin-controller.js
@@ -44,6 +44,22 @@ const getUserById=async(req,res,next)=>
 
 }
 
+const updateUserById=async(req,res,next)=>
+{
+try {
+    const id=req.params.id;
+    const updatedUserData=req.body;
+const updateddata=await User.update({_id:id},{
+    $set:updatedUserData
+});
+return res.status(200).json(updateddata);
+
+    
+} catch (error) {
+    next(error);
+}
+}
+
 
 
 
@@ -73,4 +89,4 @@ const getAllServices=async(req,res,next)=>
     }
 }
 
-module.exports={getAllUsers,getAllContacts,getAllServices,deleteUserById,getUserById};
+module.exports={getAllUsers,getAllContacts,getAllServices,deleteUserById,getUserById,updateUserById};
--- server/router/admin-router.js
+++ server/router/admin-router.js
@@ -5,7 +5,8 @@ const authMiddleware=require("../middlewares/auth-middleware");
 const adminMiddleware = require("../middlewares/admin-middleware");
 
 router.route("/users").get(authMiddleware,adminMiddleware,getAll.getAllUsers);
-router.route("/users/:id/edit").get(authMiddleware,adminMiddleware,getAll.getUserById);
+router.route("/users/:id").get(authMiddleware,adminMiddleware,getAll.getUserById);
+router.route("/users/update/:id").patch(authMiddleware,adminMiddleware,getAll.updateUserById);
 router.route("/users/delete/:id").delete(authMiddleware,adminMiddleware,getAll.deleteUserById);
 
 router.route("/contacts").get(authMiddleware,adminMiddleware,getAll.getAllContacts);
